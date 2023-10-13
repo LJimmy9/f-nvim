c289881859d9004a2d367a8831e890feb53f63d0
uj <laujimm17@gmail.com>
2023-10-13 02:39:08 +0000

    add keybinds

--- lua/keybinds/surround.lua
+++ lua/keybinds/surround.lua
@@ -1,5 +1,6 @@
 Keymap("v", "<leader>sp", 'c(<C-r>")')
 Keymap("v", "<leader>sb", 'c{<C-r>"}')
 Keymap("v", "<leader>sq", 'c[<C-r>"]')
+Keymap("v", '<leader>s"', 'c"<C-r>""')
 
-Keymap("n", "<leader>v", "_v$")
+Keymap("n", "<leader>vl", "_v$")
